68e83cf876ded06c5902cbb52610e753a31da135
Jimmy <chuneleu@gmail.com>
2019-06-02 18:35:29 -0700

    Initial Commit

---
+++ SideMenu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SideMenu
+//
+//  Created by Jimmy Leu on 2019/6/2.
+//  Copyright © 2019 Jimmy Leu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SideMenuTests/SideMenuTests.swift
@@ -0,0 +1,34 @@
+//
+//  SideMenuTests.swift
+//  SideMenuTests
+//
+//  Created by Jimmy Leu on 2019/6/2.
+//  Copyright © 2019 Jimmy Leu. All rights reserved.
+//
+
+import XCTest
+@testable import SideMenu
+
+class SideMenuTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
